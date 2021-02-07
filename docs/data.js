2765f45ff4e858f585ffe2cf082a6853acc472a7
Anatoly Khomenko <anatoly.khomenko@gmail.com>
2021-02-07 14:07:14 -0500

    updated entry point

--- docs/data.js
+++ docs/data.js
@@ -1,133 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-salon",
-      "name": "Salon",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.10888635001342806,
-          "pitch": 0.1871345137603342,
-          "rotation": 0,
-          "target": "1-salle-a-manger"
-        },
-        {
-          "yaw": 0.620704067059366,
-          "pitch": 0.09730194847391438,
-          "rotation": 0,
-          "target": "3-entre"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-salle-a-manger",
-      "name": "Salle a manger",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.5538511437635414,
-          "pitch": 0.23422237251900846,
-          "rotation": 0,
-          "target": "2-cuisine"
-        },
-        {
-          "yaw": 2.8239876549628598,
-          "pitch": 0.4521013950787349,
-          "rotation": 0,
-          "target": "0-salon"
-        },
-        {
-          "yaw": 1.5664102395596284,
-          "pitch": 0.2605144414193994,
-          "rotation": 0,
-          "target": "3-entre"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-cuisine",
-      "name": "Cuisine",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.05376738183302976,
-          "pitch": -0.048663606040285856,
-          "rotation": 0,
-          "target": "3-entre"
-        },
-        {
-          "yaw": -2.9654102651349596,
-          "pitch": 0.26494629955267435,
-          "rotation": 0,
-          "target": "1-salle-a-manger"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-entre",
+      "id": "0-entre",
       "name": "Entrée",
       "levels": [
         {
@@ -173,13 +47,139 @@ var APP_DATA = {
           "yaw": -0.9036874036040707,
           "pitch": 0.0709408635717459,
           "rotation": 0,
-          "target": "2-cuisine"
+          "target": "3-cuisine"
         },
         {
           "yaw": -1.814940609041635,
           "pitch": 0.1410961664561654,
           "rotation": 0,
-          "target": "1-salle-a-manger"
+          "target": "2-salle-a-manger"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.5395740158690323,
+        "pitch": 0.20577638465014658,
+        "fov": 1.3517668344523721
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10888635001342806,
+          "pitch": 0.1871345137603342,
+          "rotation": 0,
+          "target": "2-salle-a-manger"
+        },
+        {
+          "yaw": 0.620704067059366,
+          "pitch": 0.09730194847391438,
+          "rotation": 0,
+          "target": "0-entre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salle-a-manger",
+      "name": "Salle a manger",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.4770566622117798,
+        "pitch": 0.16491555380319056,
+        "fov": 1.3517668344523721
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5538511437635414,
+          "pitch": 0.23422237251900846,
+          "rotation": 0,
+          "target": "3-cuisine"
+        },
+        {
+          "yaw": 2.8239876549628598,
+          "pitch": 0.4521013950787349,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": 1.5664102395596284,
+          "pitch": 0.2605144414193994,
+          "rotation": 0,
+          "target": "0-entre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cuisine",
+      "name": "Cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.021444545826675,
+        "pitch": 0.09707493760436314,
+        "fov": 1.3517668344523721
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05376738183302976,
+          "pitch": -0.048663606040285856,
+          "rotation": 0,
+          "target": "0-entre"
+        },
+        {
+          "yaw": -2.9654102651349596,
+          "pitch": 0.26494629955267435,
+          "rotation": 0,
+          "target": "2-salle-a-manger"
         }
       ],
       "infoHotspots": []
@@ -204,16 +204,16 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -1.015626988037429,
+        "pitch": 0.3646423430316368,
+        "fov": 1.3517668344523721
       },
       "linkHotspots": [
         {
           "yaw": 2.9107985891114456,
           "pitch": 0.21443949901239634,
           "rotation": 0,
-          "target": "3-entre"
+          "target": "0-entre"
         }
       ],
       "infoHotspots": []
@@ -238,16 +238,16 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -2.5496115771801193,
+        "pitch": 0.23073207835267695,
+        "fov": 1.3517668344523721
       },
       "linkHotspots": [
         {
           "yaw": 0.5350778324668894,
           "pitch": 0.1699159865169726,
           "rotation": 0,
-          "target": "3-entre"
+          "target": "0-entre"
         }
       ],
       "infoHotspots": []
@@ -272,16 +272,16 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.5526875964648497,
+        "pitch": 0.10543781308728484,
+        "fov": 1.3517668344523721
       },
       "linkHotspots": [
         {
           "yaw": 2.7777724949890494,
           "pitch": 0.10987026331966732,
           "rotation": 0,
-          "target": "3-entre"
+          "target": "0-entre"
         }
       ],
       "infoHotspots": []
